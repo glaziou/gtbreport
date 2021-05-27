0ec58a892c164bd87c6eb8fb0f1a6c936fcca181
Philippe Glaziou <glaziou@gmail.com>
2021-05-27 08:02:06 +0200

    updated theme_gtb() (from Hazim)

--- R/theme_gtb.R
+++ R/theme_gtb.R
@@ -2,59 +2,65 @@
 #'
 #' @param base_size base font size, given in pts.
 #' @param base_family base font family
+#' @param axis_text_size axis font size
 #' @author: Hazim Timimi
-#' @import ggplot2
+#' @examples
+#' p <- ggplot(mtcars, aes(wt, mpg)) +
+#'      geom_point() + facet_wrap(~ cyl)
+#' print(p)
+#' p2 <- p + theme_gtb()
+#' print(p2)
 #'
+#' @import ggplot2
 #' @export
 #'
-theme_gtb <- function(base_size = 14,
-                            base_family = "") {
+theme_gtb <- function(base_size=14, base_family="", axis_text_size=8) {
+
+  faint_gray <- "#CCCCCC"
   gray <- "#BCBCBC"
   charcoal <- "#222222"
+
   ggplot2::theme(
-    #Text format:
-    plot.margin = margin(30, 5, 30, 5),
-    #Legend format
+    # Text format:
+    plot.margin = margin(30,5,30,5),
+
+    # Legend format
     legend.position = "bottom",
     legend.text.align = 0,
-    legend.background = element_blank(),
-    legend.title = element_blank(),
-    legend.key = element_blank(),
-    legend.text = element_text(
-      family = base_family,
-      size = 14,
-      color = charcoal
-    ),
-    #Axis format
-    axis.title = element_text(
-      family = base_family,
-      size = 14,
-      color = charcoal
-    ),
-    axis.text = element_text(
-      family = base_family,
-      size = 8,
-      color = charcoal
-    ),
-    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
-    axis.ticks.x = element_line(colour = gray),
-    axis.ticks.y = element_blank(),
-    axis.line.x = ggplot2::element_line(color = gray, size = 0.25),
-    axis.line.y = element_blank(),
-    #Grid lines
+    legend.background = ggplot2::element_blank(),
+    legend.title = ggplot2::element_blank(),
+    legend.key = ggplot2::element_blank(),
+    legend.text = ggplot2::element_text(family=base_family,
+                               size=base_size,
+                               colour=charcoal),
+
+    # Axis format
+    axis.title = ggplot2::element_text(family=base_family,
+                              size=base_size,
+                              colour=charcoal),
+
+    axis.text = ggplot2::element_text(family=base_family,
+                             size=axis_text_size,
+                             colour=charcoal),
+    axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
+    axis.ticks.x = ggplot2::element_line(colour = gray),
+    axis.ticks.y = ggplot2::element_blank(),
+    axis.line.x = ggplot2::element_line(colour=gray, size = 0.25),
+    axis.line.y = ggplot2::element_blank(),
+
+    # Grid lines
     panel.grid.minor = ggplot2::element_blank(),
-    panel.grid.major.y = ggplot2::element_blank(),
+    panel.grid.major.y = ggplot2::element_line(colour = faint_gray, size = 0.1),
     panel.grid.major.x = ggplot2::element_blank(),
+
     # Have plain background in headings of facet sub-plots
-    strip.background = element_rect(fill = "white", colour = NA),
-    strip.text = element_text(
-      family = base_family,
-      size = 14,
-      face = "bold",
-      color = charcoal
-    ),
-    #Blank background
+    strip.background = ggplot2::element_rect(fill="white", colour=NA),
+    strip.text = ggplot2::element_text(family=base_family,
+                              size=base_size,
+                              face="bold",
+                              colour=charcoal),
+
+    # Blank background
     panel.background = ggplot2::element_blank(),
   )
 }
-
